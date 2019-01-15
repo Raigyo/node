3fd0a7a598ef99640a868590039c6d99d7a62a2a
Raigyo <vincent.chilot@gmail.com>
2019-01-15 14:48:33 +0100

    v3

--- test.js
+++ test.js
@@ -1,4 +1,7 @@
+//display data
 console.log("Hello World!");
-
-
+//import 'read-line' library
 const readlineSync = require("readline-sync");
+//The let keyword allows us to define a new variable
+let userName = readlineSync.question('Can you give me your name please?');
+console.log("Hello " + userName);
