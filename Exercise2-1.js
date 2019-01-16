a446cfda0d28458e4ff98511a0cb5bdb08be4113
Raigyo <vincent.chilot@gmail.com>
2019-01-16 15:39:17 +0100

    v1

---
+++ Exercise2-1.js
@@ -0,0 +1,10 @@
+/*Ask the user to enter its age. If its age is at least 18 display "You are an adult".
+If it's not the case display "You are not yet an adult".*/
+const readlineSync = require("readline-sync");
+let age = readlineSync.question("What is your age?");
+if (age >= 18){
+console.log("You're an adult!");
+}
+else {
+console.log("You're' not yet an adult")  
+}
