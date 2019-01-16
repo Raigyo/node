58024c34e5e0039b9f3a5139d5df2ccabce673dd
Raigyo <vincent.chilot@gmail.com>
2019-01-16 15:42:00 +0100

    update

--- Exercise2-1.js
+++ Exercise2-1.js
@@ -6,5 +6,5 @@ if (age >= 18){
 console.log("You're an adult!");
 }
 else {
-console.log("You're' not yet an adult")  
+console.log("You're' not yet an adult")
 }
---
+++ Exercise2-2.js
@@ -0,0 +1,7 @@
+/*Ask the user to enter three numbers: min, max and current.
+Display if current is between min and max.
+Bonus: if min is greater than max, display an error message to explain
+the user he's doesn't understand anything then exit the program.
+(It must not ask any other question.)
+Bonus 2: be polite in the error messages. (facultative)*/
+const readlineSync = require("readline-sync");
