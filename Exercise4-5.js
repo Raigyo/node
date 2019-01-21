56dd5d7116bf6539f1d5802fe7d2cd157724722c
Raigyo <vincent.chilot@gmail.com>
2019-01-21 15:59:03 +0100

    update

--- Exercise4-5.js
+++ Exercise4-5.js
@@ -6,9 +6,6 @@ Note: You probably need to make some search on Google to learn the mathematical 
 You will also probably need to search for useful functions in JavaScript
 to help you making complex mathematical formulas...
 https://stackoverflow.com/questions/20916953/get-distance-between-two-points-in-canvas/20916980
-var a = x1 - x2;
-var b = y1 - y2;
-var c = Math.sqrt( a*a + b*b );
 */
 
 const readlineSync = require("readline-sync");
@@ -17,11 +14,12 @@ let coordY1 = readlineSync.question("Coordonate Y1:");
 let coordX2 = readlineSync.question("Coordonate X2:");
 let coordY2 = readlineSync.question("Coordonate Y2:");
 
+//pythagoras theorem
 function calcDistance (x1,y1,x2,y2){
     let a = x1 - x2;
     let b = y1 - y2;
     let c = Math.sqrt( a*a  + b*b );
-    console.log("Distance netween A & b:" +c);
+    console.log("Distance between A & b: " +c);
 }
 
-calcDistance (coordX1,coordY1,coordX2,coordY2)
+calcDistance (coordX1,coordY1,coordX2,coordY2);
