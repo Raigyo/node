f055df86fd7c4601a82e71f8c21ee4c3d68ceb93
Raigyo <vincent.chilot@gmail.com>
2019-01-21 16:35:45 +0100

    update

---
+++ Exercise4-6.js
@@ -0,0 +1,28 @@
+/*
+Create a function factorial(a) that returns the factorial of a number.
+That function must be recursive.
+
+En mathématiques, la factorielle d'un entier naturel n est le produit des nombres entiers
+strictement positifs inférieurs ou égaux à n: n!
+https://onechapteraday.fr/javascript-recursive-functions/
+*/
+const readlineSync = require("readline-sync");
+let fact= readlineSync.question("Enter a number:");
+
+function factorial(x) {
+  let result = 1;
+  for (var i = 1; i <= x; i++)
+    result = result * i;
+    return result;
+
+}
+console.log(factorial(fact));
+
+function factorial2(x) {
+  // This is the base case.
+  if (x === 0) return 1;
+  // This is the recursive one.
+  else return x * factorial2(x - 1);
+    //console.log(x);
+}
+console.log(factorial2(fact));
