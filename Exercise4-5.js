15a9a6b950013a469fc6d252e61c422d60f1802a
Raigyo <vincent.chilot@gmail.com>
2019-01-21 15:56:34 +0100

    update

---
+++ Exercise4-5.js
@@ -0,0 +1,27 @@
+/*Create a function named calcDistance which takes the coordinates
+of two different points A and B in a 2D space.
+That function must return the distance between those two points.
+Create a program to use that function.
+Note: You probably need to make some search on Google to learn the mathematical formula to do that.
+You will also probably need to search for useful functions in JavaScript
+to help you making complex mathematical formulas...
+https://stackoverflow.com/questions/20916953/get-distance-between-two-points-in-canvas/20916980
+var a = x1 - x2;
+var b = y1 - y2;
+var c = Math.sqrt( a*a + b*b );
+*/
+
+const readlineSync = require("readline-sync");
+let coordX1 = readlineSync.question("Coordonate X1:");
+let coordY1 = readlineSync.question("Coordonate Y1:");
+let coordX2 = readlineSync.question("Coordonate X2:");
+let coordY2 = readlineSync.question("Coordonate Y2:");
+
+function calcDistance (x1,y1,x2,y2){
+    let a = x1 - x2;
+    let b = y1 - y2;
+    let c = Math.sqrt( a*a  + b*b );
+    console.log("Distance netween A & b:" +c);
+}
+
+calcDistance (coordX1,coordY1,coordX2,coordY2)
