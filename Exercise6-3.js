20d8ba056913067986aa675537539f692fbb05c7
Raigyo <vincent.chilot@gmail.com>
2019-01-23 16:56:03 +0100

    update

--- Exercise6-3.js
+++ Exercise6-3.js
@@ -2,6 +2,8 @@
 Create a program that will re-use the Rectangle class you created previously.
 It should generate 1000 random instances of Rectangle with random positions and sizes.
 Then it will display all colliding rectangles amongst those that were generated that way.
+Math.floor(Math.random() * 1000) + 1; // returns a random integer from 1 to 1000
+Math.floor(Math.random() * 1000);     // returns a random integer from 0 to 999
 */
 class Rectangle {
   constructor(topLeftXPos, topLeftYPos, width, lenght) {
@@ -19,19 +21,29 @@ class Rectangle {
     this.topLeftYPos < otherRectangle.topLeftYPos + otherRectangle.lenght &&
     this.lenght + this.topLeftYPos > otherRectangle.topLeftYPos) {
     //return true;
-    console.log("true");
+    console.log(stringify.this+"true");
     }
     else {
     //return false;
-      console.log("false");
+      console.log(stringify.this+"false");
     }
-    /*console.log(this.topLeftXPos);
-    console.log(otherRectangle.topLeftXPos);*/
   }//method
 }//class
 
-let rect1 = new Rectangle (0,50,75,75);
-let rect2 = new Rectangle (0,20,80,85);
-let rect3 = new Rectangle (500,400,5,3);
-rect1.collides(rect2); // output true
-rect2.collides(rect1); // output true
+//Create 1000 rectangles with random x, y, xPos, yPos
+//the test if they collapse
+function thousandRect()  {
+  let myObject ={
+    rect :[]
+  }
+  for (i=0; i<=999; i++){
+    myObject.rect[i] = new Rectangle (Math.floor(Math.random() * 1000),Math.floor(Math.random() * 1000),Math.floor(Math.random() * 1000),Math.floor(Math.random() * 1000));
+  }
+  //collapse test
+  for (i=0; i<=999; i++){
+    for (j=0; j<=999; j++){
+      myObject.rect[i].collides(myObject.rect[j]);
+    }
+  }
+}
+thousandRect();
