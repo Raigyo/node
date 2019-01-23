d62ef2b626df384bbbbce4300f28b5bd037383e2
Raigyo <vincent.chilot@gmail.com>
2019-01-23 10:36:29 +0100

    update

--- Exercise5-2.js
+++ Exercise5-2.js
@@ -1,11 +1,19 @@
 /*
-
-Create a function named randomizeCast(tvSerie) that will take
+1.Create a function named randomizeCast(tvSerie) that will take
 as argument the data structure you defined in the previous exercise
 and return a list of the same cast but in a random order.
-Create a program that will use randomizeCast(tvSerie) and askTvSerie()
+2.Create a program that will use randomizeCast(tvSerie) and askTvSerie()
 to ask the user about a TV serie then display a randomized
 list of the previous cast that will form the new cast of your next serie.
+------
+Fisher–Yates shuffle
+To shuffle an array a of n elements (indices 0..n-1):
+for i from n−1 downto 1 do
+     j ← random integer such that 0 ≤ j ≤ i
+     exchange a[j] and a[i]
+------
+https://www.w3resource.com/javascript-exercises/javascript-array-exercise-17.php
+https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
 */
 const readlineSync = require("readline-sync");
 function askTvSerie(){
@@ -25,11 +33,25 @@ function askTvSerie(){
       i++;
     }
   }
-  //console.log(myObject);
+  //console.log(myObject)
+  console.log("Old object: " + JSON.stringify(myObject));
   randomizeCast(myObject);
 }
 askTvSerie();
 
-randomizeCast() {
-  console.log(myObject);
+function randomizeCast(dataSeries){
+  let ctr = dataSeries.casting.length, temp, index;
+  // While there are elements in the array
+  while (ctr > 0) {
+      // Pick a random index
+      index = Math.floor(Math.random() * ctr);
+      // Decrease ctr by 1
+      ctr--;
+      // And swap the last element with it
+      temp = dataSeries.casting[ctr];
+      dataSeries.casting[ctr] = dataSeries.casting[index];
+      dataSeries.casting[index] = temp;
+  }
+  //return dataSeries.casting;
+  console.log("Mixed object: " + JSON.stringify(dataSeries));
 }
