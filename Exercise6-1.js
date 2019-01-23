278bb738321fb99b2d0f7445bb826465a8580855
Raigyo <vincent.chilot@gmail.com>
2019-01-23 12:18:47 +0100

    update

--- Exercise6-1.js
+++ Exercise6-1.js
@@ -6,16 +6,22 @@ Test its method and accessors by modifying the values and checking if everything
 -----
 Circle surface: π × R2.
 https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Math/PI
+https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Math/pow
 */
 class Circle {
   constructor(xPos, yPos, radius) {
-    this.xPos = xPos;
-    this.yPos = yPos;
-    this.radius = radius;
-    }//constructor
-    move(xOffset, yOffset) {
-    console.log(this.xPos + " move to " + xOffset);
-    console.log(this.yPos + " move to " + yOffset);
-    }//method
-  }//class
-new Circle(10,12,20).move(50,60);
+  this.xPos = xPos;
+  this.yPos = yPos;
+  this.radius = radius;
+  }//constructor
+  get surface() {
+  return Math.pow(this.radius, 2) * Math.PI;
+  }//getter accessor
+  move(xOffset, yOffset) {
+  console.log("x: " + this.xPos + " move to " + xOffset);
+  console.log("y: " + this.yPos + " move to " + yOffset);
+  }//method
+}//class
+let circle1 = new Circle (10,12,20);
+console.log("Surface: " + circle1.surface);
+circle1.move(50,60);
