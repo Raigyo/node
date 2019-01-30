407b40154d03c8fd00187c4ef85f1acd7401effb
Raigyo <vincent.chilot@gmail.com>
2019-01-30 16:13:47 +0100

    update

--- Exercise7-4.js
+++ Exercise7-4.js
@@ -21,14 +21,16 @@ Remove a pizza flavor: Ask the user for the number of the pizza flavor
 in the list he wants to remove.
 Each time one of the action has been performed it should return to the menu.
 (Except of course if we select to exit the program.)
-Bonus: Save the list of pizza flavors on the file system and reload it each time we launch the application.
+Bonus: Save the list of pizza flavors on the file system
+and reload it each time we launch the application.
 */
 const readlineSync = require("readline-sync");
 let choice;
 let numPizza = 0;
 let exit = false;
-let myMenu = ["cheese" , "margarita"];
-//ask number
+let myMenu = [];
+
+//ask a choice in the menu
 function progrMenu () {
   console.log("Hello! Welcome to the Pizza Flavors Manager. \n Please choose your actions: \n 1 - List all the pizza flavors \n 2 - Add a new pizza flavor \n 3 - Remove a pizza flavor \n 4 - Exit this program");
   choice = readlineSync.question("Enter your choice [1 - 4]: ");
@@ -39,17 +41,19 @@ function progrMenu () {
 }
 progrMenu();
 
+//switchcase with operations 1 to 4
 function switchCase (choice) {
   switch(parseInt(choice)) {
     case 1:
       console.log("List all the pizza flavors:");
-      console.log(myMenu);
+      for (let i = 0; i < myMenu.length; i++) {
+        console.log(i+1 + ":" + myMenu[i]);
+      };
       choice = 0;
       progrMenu();
     case 2:
       newFlavor = readlineSync.question("Add a new pizza flavor: ");
       myMenu.push(newFlavor);
-      console.log(myMenu);
       choice = 0;
       progrMenu();
     case 3:
@@ -57,10 +61,17 @@ function switchCase (choice) {
         console.log(i+1 + ":" + myMenu[i]);
       };
       newFlavor = readlineSync.question("Delete a pizza flavor: ");
-      myMenu.splice(parseInt(newFlavor-1), 1);
+      console.log(newFlavor);
+      if (newFlavor <= myMenu.length || newFlavor > 1 || newFlavor  % 1 !== 0){
+        myMenu.splice(parseInt(newFlavor-1), 1);
+      }
+      else {
+        console.log("Not a valid choice, back to menu")
+      };
+      choice = 0;
       progrMenu();
     case 4:
-
-    break;
+      break;
+      //localStorage["myMenu"] = JSON.stringify(myMenu);
   }
 }
