90fb4a74c397bb950b9614004af11e04c98c1ae5
Raigyo <vincent.chilot@gmail.com>
2019-01-30 16:42:10 +0100

    update

---
+++ Exercise7-5.js
@@ -0,0 +1,43 @@
+/*
+Exercise 7.5 - Array Sort
+Create a program that can sort an array without
+using Array.sort() or a similar method.
+Test it with multiple arrays of numbers.*/
+
+//create 2 arrays
+var arrayNumbers = [];
+var fruitsList = ["Banana", "Tomato,", "Orange", "Apple", "Mango", "Lemon", "Peach"];
+for (var i = 0; i < 10 ; i++) {
+  var newNumber = Math.floor(Math.random() * 1000) + 1;
+  arrayNumbers.push(newNumber);
+
+}
+
+//don't use sort
+/*
+fruitsList.sort();
+console.log(fruitsList);
+arrayNumbers.sort();
+console.log(arrayNumbers);
+*/
+
+//BOBBLE sort: https://initjs.org/bubble-sort-in-javascript-19fa6fdfbb46
+function sort(values) {
+  var origValues = values.slice();
+  var length = origValues.length - 1;
+  do {
+    var swapped = false;
+    for(var i = 0; i < length; ++i) {
+      if (origValues[i] > origValues[i+1]) {
+        var temp = origValues[i];
+        origValues[i] = origValues[i+1];
+        origValues[i+1] = temp;
+        swapped = true;
+      }
+    }
+  }
+  while(swapped === true);
+  console.log(origValues);
+}
+sort(fruitsList);
+sort(arrayNumbers);
